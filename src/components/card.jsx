b738c0a8246a1178f0d8f469414f75946c9ae61e
Atharv <atharvsingh753@gmail.com>
2024-07-15 19:53:03 +0530

    intial commit

---
+++ src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FaFileAlt } from "react-icons/fa"; 
+
+function card() {
+  return (
+          <div className='w-60 h-72 rounded-[20px] bg-blue-500 text-white p-5'> 
+      <FaFileAlt className=''/>
+    </div>
+  )
+}
+
+export default card;
+ 
